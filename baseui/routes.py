176f0fdde1d4d2210b8202a4ade501604512a8e6
erickpeirson <brp53@cornell.edu>
2017-12-01 13:45:39 -0500

    ARXIVNG-163 added minimal flask app for verification/testing

---
+++ baseui/routes.py
@@ -0,0 +1,11 @@
+"""Provides routes for verifying baseui templates."""
+
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('ui', __name__, url_prefix='')
+
+
+@blueprint.route('/test', methods=['GET'])
+def test_page() -> tuple:
+    """Render the test page."""
+    return render_template("baseui/testpage.html")
