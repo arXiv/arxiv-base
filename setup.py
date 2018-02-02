66cf36b63850c440200bb8e9b5b6c2f958631e9f
erickpeirson <brp53@cornell.edu>
2018-02-02 16:43:02 -0500

    ARXIVNG-109 updated search box

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-base',
-    version='0.1',
+    version='0.1.3',
     packages=find_packages(exclude=['tests.*']),
     zip_safe=False,
     include_package_data=True
