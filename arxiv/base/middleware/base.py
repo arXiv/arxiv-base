0b78d5c44f9ac8f178e9ca144820b5a28d6538aa
erickpeirson <brp53@cornell.edu>
2018-03-29 13:45:52 -0400

    mypy fix

--- arxiv/base/middleware/base.py
+++ arxiv/base/middleware/base.py
@@ -42,12 +42,13 @@ class BaseMiddleware(object):
             Iterable that generates the HTTP response. See
             https://www.python.org/dev/peps/pep-0333/#the-application-framework-side
         """
+
         if hasattr(self, 'before'):
-            environ, start_response = self.before(environ, start_response)
+            environ, start_response = self.before(environ, start_response)  # type: ignore
         response: Iterable = self.app(environ, start_response)
 
         if hasattr(self, 'after'):
-            response = self.after(response)
+            response = self.after(response)       # type: ignore
         return response
 
     @property
