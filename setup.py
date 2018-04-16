acca51e54877892fb7e18b3ff47519b1717c8456
erickpeirson <brp53@cornell.edu>
2018-04-16 10:11:05 -0400

    incremented patch version to 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-base',
-    version='0.5.1',
+    version='0.5.2',
     packages=find_packages(exclude=['tests.*']),
     zip_safe=False,
     include_package_data=True
