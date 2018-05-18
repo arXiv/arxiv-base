84012ecf29f4dd40fe774da98e9764cd0f178b9e
erickpeirson <brp53@cornell.edu>
2018-05-18 10:56:28 -0400

    incremented version to 0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-base',
-    version='0.6.1',
+    version='0.7',
     packages=find_packages(exclude=['tests.*']),
     zip_safe=False,
     include_package_data=True
