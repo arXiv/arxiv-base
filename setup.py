544d48e4fc7b431c610a77cda80beb231c598175
erickpeirson <brp53@cornell.edu>
2018-07-03 15:09:44 -0400

    ARXIVNG-882 added pipfile requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from setuptools import setup, find_packages
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+
 
 setup(
     name='arxiv-base',
@@ -9,5 +14,6 @@ setup(
     packages=[f'arxiv.{package}' for package
               in find_packages('arxiv', exclude=['*test*'])],
     zip_safe=False,
+    install_requires=convert_deps_to_pip(pfile['packages'], r=False),
     include_package_data=True
 )
