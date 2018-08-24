6c121f1f5bd235d6fee9df73ce213d8e8162d34c
erickpeirson <brp53@cornell.edu>
2018-08-24 17:10:03 -0400

    minor

--- arxiv/base/routes.py
+++ arxiv/base/routes.py
@@ -13,7 +13,7 @@ from arxiv import status
 from arxiv.base.exceptions import NotFound, Forbidden, Unauthorized, \
     MethodNotAllowed, RequestEntityTooLarge, BadRequest, InternalServerError
 
-from . import messages
+from . import alerts
 
 blueprint = Blueprint('ui', __name__, url_prefix='')
 
@@ -24,15 +24,15 @@ def test_page() -> Response:
     rendered = render_template("base/styleguide.html", pagetitle='Home')
     response = make_response(rendered, status.HTTP_200_OK)
 
-    # Demonstrate flash messages. To see these messages, reload the page.
+    # Demonstrate flash alerts. To see these alerts, reload the page.
     help_url = url_for('help')
-    messages.flash_warning(f'This is a warning, see <a href="{help_url}">the'
+    alerts.flash_warning(f'This is a warning, see <a href="{help_url}">the'
                            f' docs</a> for more information',
                            title='Warning title', safe=True)
-    messages.flash_info('This is some info', title='Info title')
-    messages.flash_failure('This is a failure', title='Failure title')
-    messages.flash_success('This is a success', title='Success title')
-    messages.flash_warning('This is a warning that cannot be dismissed',
+    alerts.flash_info('This is some info', title='Info title')
+    alerts.flash_failure('This is a failure', title='Failure title')
+    alerts.flash_success('This is a success', title='Success title')
+    alerts.flash_warning('This is a warning that cannot be dismissed',
                            dismissable=False)
     return response
 
