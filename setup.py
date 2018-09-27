bfc1eb5fbb2824429c6b07c1d461e190e32448a4
erickpeirson <brp53@cornell.edu>
2018-09-27 08:17:33 -0400

    Fixed a bug in arxiv.converter (wrong exception raised)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-base',
-    version='0.11.1rc5',
+    version='0.11.1rc6',
     packages=[f'arxiv.{package}' for package
               in find_packages('arxiv', exclude=['*test*'])],
     zip_safe=False,
