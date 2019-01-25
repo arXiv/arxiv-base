f142600b6e2040aeae95e868eb385c56ff3ae172
erickpeirson <brp53@cornell.edu>
2019-01-25 11:26:34 -0500

    incremented minor version to 0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-base',
-    version='0.12.1rc2',
+    version='0.13',
     packages=[f'arxiv.{package}' for package
               in find_packages('arxiv', exclude=['*test*'])],
     zip_safe=False,
