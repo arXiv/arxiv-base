d46eea785f2d2439db53d5bdb88d318aad1ce721
Martin Lessmeister <mhl10@cornell.edu>
2019-02-19 09:55:18 -0500

    Add abstract_lf_to_br filter

--- arxiv/base/filters.py
+++ arxiv/base/filters.py
@@ -1,12 +1,38 @@
 """Template filters."""
 
+import re
 from functools import partial
+from typing import Union
 from flask import Flask
+from jinja2 import Markup, escape
 from arxiv.base.urls import urlize, url_for_doi, canonical_url, \
     clickthrough_url
 from arxiv.util.tex2utf import tex2utf
 from arxiv.taxonomy import get_category_display, get_archive_display, \
     get_group_display
+
+JinjaFilterInput = Union[Markup, str]
+"""
+   Jinja filters will receive their text input as either
+   a Markup object or a str. It is critical for proper escaping to
+   to ensure that str is correctly HTML escaped.
+
+   Markup is decoded from str so this type is redundant but
+   the hope is to make it clear what is going on to arXiv developers.
+"""
+
+
+def abstract_lf_to_br(text: JinjaFilterInput) -> Markup:
+    """Lines that start with two spaces should be broken."""
+    if hasattr(text, '__html__'):
+        etxt = text
+    else:
+        etxt = Markup(escape(text))
+
+    # if line starts with spaces, replace the white space with <br />
+    br = re.sub(r'((?<!^)\n +)', '\n<br />', etxt)
+    dedup = re.sub(r'\n\n', '\n', br)  # skip if blank
+    return Markup(dedup)
 
 
 def register_filters(app: Flask) -> None:
@@ -18,6 +44,7 @@ def register_filters(app: Flask) -> None:
     app : :class:`Flask`
 
     """
+    app.template_filter('abstract_lf_to_br')(abstract_lf_to_br)
     app.template_filter('urlize')(urlize)
     app.template_filter('tex2utf')(partial(tex2utf, letters=True))
     app.template_filter('tex2utf_no_symbols')(partial(tex2utf, letters=False))
