fa89388863f95c84e4a14fe147e8b8d71cab53be
Brian D. Caruso <bdc34@cornell.edu>
2020-02-14 10:07:12 -0500

    get_version now checks version with pkg_resources
    This will get the version for an package installed from a directory,
    zip or pypi.

--- arxiv/release/dist_version.py
+++ arxiv/release/dist_version.py
@@ -12,7 +12,7 @@ import sys
 import pathlib
 from subprocess import Popen, PIPE
 from datetime import datetime
-
+import pkg_resources
 
 def get_version(dist_name):
     """Get the version written by write_version(), or the git describe version.
@@ -21,14 +21,16 @@ def get_version(dist_name):
     ----------
     dist_name: str
         Which arxiv distribution to get. ex arxiv-base
-        arxiv-filemanager.  These will be mapped to arxiv.base.version
-        and arxiv.filemanager.version.
+        arxiv-filemanager.  This should be the name from setup.py or
+        pypi.  These will be mapped to arxiv.base.version and
+        arxiv.filemanager.version.
 
     Returns
     -------
     str
         The version.__version__ value if it exists or the git describe
         version if it exists or the string 'no-git-or-release-version'
+
     """
     # TODO We might want to make it an error if we are under git
     # and there is a version.py file? It doesn't seem like a good state.
@@ -38,6 +40,10 @@ def get_version(dist_name):
         dist_version = getattr(__import__(pkg, fromlist=[name]), name)
         return dist_version
     except ModuleNotFoundError:
+        pass
+    try:
+        return get_pkg_version(dist_name)
+    except Exception:
         pass
     try:
         return get_git_version()
@@ -69,12 +75,24 @@ def write_version(dist_name, version):
     with open(path, 'w+') as ff:  # overwrite existing version
         when = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         ff.write("#  Created by tag_check.write_version\n")
-        ff.write("#  NEVER CHECK THIS INTO GIT.\n")
+        ff.write("#  NEVER CHECK THIS version.py file INTO GIT.\n")
+        ff.write("#  It should be generated when the package is build for distribution.\n")
         ff.write(f"__when__ = '{when}'\n")
         ff.write(f"__version__ = '{version}'\n")
     return path
 
 
+def get_pkg_version(pkg):
+    """Get the python package version.
+    
+    pkg needs to be the package name from setup.py or the name used to
+    install from pypi.
+    """
+    try:
+        return pkg_resources.get_distribution(pkg).version
+    except Exception:
+        return None
+    
 def get_git_version(abbrev=7):
     """Get the current version using `git describe`."""
     try:
