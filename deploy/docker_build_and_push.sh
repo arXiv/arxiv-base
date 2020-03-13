252e71666dd708358b12b188a8e0110877b43704
Brian D. Caruso <bdc34@cornell.edu>
2020-03-13 11:09:32 -0400

    Adding travis docker build and push ARXIVNG-3035

--- deploy/docker_build_and_push.sh
+++ deploy/docker_build_and_push.sh
@@ -9,7 +9,9 @@ if [ -z $2 ]; then
 fi
 
 echo "$DOCKERHUB_PASSWORD" | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
+echo "docker build $1 -t $2"
 docker build $1 -t $2
+echo "docker push $2"
 docker push $2
 docker logout
 
