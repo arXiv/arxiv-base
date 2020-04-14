30d31e4bfcae8a239e748fb9b13d8d727452a942
Brian Caruso <bdc34@cornell.edu>
2020-04-14 15:26:42 -0400

    Adding module to do docker build and push (#183)
    * Removing docker_build_and_push.sh: It is replaced the Python module 
     arxiv.release.docker_build_push so it can be reused in other repositories.
    * Handle None from get_pkg_version
    * Adding confirmation to upload_static_assets.py
    * Adding back in docker build and push
    * Adds build and push of base and fourohfour back into travis ci.
    
    ARXIVNG-3035

---
+++ arxiv/release/docker_build_push.py
@@ -0,0 +1,37 @@
+"""Runs docker login, build and push commands.
+
+For use in travis ci."""
+
+import os
+import sys
+from subprocess import call
+
+try:
+    tag = os.environ['TRAVIS_TAG']
+    user = 'arxiv'
+    context = sys.argv[1]
+    label = sys.argv[2]
+except:
+    print('Example of use:')
+    print('TRAVIS_TAG=1.2.3 DOCKERHUB_PASSWORD=1234 DOCKERHUB_USERNAME=frank python -m arxiv.release.docker_build_push ./fourohfour fourohfour')
+    exit(1)
+
+try:    
+    labeltag = f"{user}/{label}:{tag}"
+    login = f'docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"'
+    build = f"""docker build -t {labeltag} {context}"""
+    push = f"""docker push {labeltag}"""
+    logout = "docker logout"    
+    cmds = ' ' + ' && '.join([login, build, push, logout])
+
+    #Running all the commands in a single subshell to keep docker login
+    retcode = call(cmds, shell=True)
+    if retcode < 0:
+        print("Child was terminated by signal", -retcode, file=sys.stderr)
+        exit(1)
+    else:
+        print("Child returned", retcode, file=sys.stderr)
+        exit(retcode)
+except OSError as e:
+    print("Execution failed:", e, file=sys.stderr)
+    exit(1)
