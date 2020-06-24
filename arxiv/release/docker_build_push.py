f1f7c70fb5d9467d71e0fbd84cf5511895bddb07
Martin Lessmeister <mhl10@cornell.edu>
2020-06-24 13:16:44 -0400

    style

--- arxiv/release/docker_build_push.py
+++ arxiv/release/docker_build_push.py
@@ -1,6 +1,7 @@
 """Runs docker login, build and push commands.
 
-For use in travis ci."""
+For use in travis ci.
+"""
 
 import os
 import sys
@@ -11,20 +12,20 @@ try:
     user = 'arxiv'
     context = sys.argv[1]
     label = sys.argv[2]
-except:
+except Exception:
     print('Example of use:')
     print('TRAVIS_TAG=1.2.3 DOCKERHUB_PASSWORD=1234 DOCKERHUB_USERNAME=frank python -m arxiv.release.docker_build_push ./fourohfour fourohfour')
     exit(1)
 
-try:    
+try:
     labeltag = f"{user}/{label}:{tag}"
-    login = f'docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"'
+    login = 'docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"'
     build = f"""docker build -t {labeltag} {context}"""
     push = f"""docker push {labeltag}"""
-    logout = "docker logout"    
+    logout = "docker logout"
     cmds = ' ' + ' && '.join([login, build, push, logout])
 
-    #Running all the commands in a single subshell to keep docker login
+    # Running all the commands in a single subshell to keep docker login
     retcode = call(cmds, shell=True)
     if retcode < 0:
         print("Child was terminated by signal", -retcode, file=sys.stderr)
