4005d89048e267b07ac6fa1e553e98a3310a0e47
Martin Lessmeister <mhl10@cornell.edu>
2020-11-02 20:38:15 -0500

    update license notes and labels

--- arxiv/license/__init__.py
+++ arxiv/license/__init__.py
@@ -5,43 +5,68 @@ LICENSE_ICON_BASE_URI = '/icons/licenses'
 LICENSES = {
     # key is the license URI
     'http://creativecommons.org/licenses/by/4.0/': {
-        'label': 'Creative Commons Attribution license',
+        'label': 'CC BY: Creative Commons Attribution',
+        'note': 'This license allows reusers to distribute, remix, adapt, and '
+                'build upon the material in any medium or format, so long as '
+                'attribution is given to the creator. The license allows for '
+                'commercial use. '
         'order': 1,
         'is_current': True,
         'icon_uri': f'{LICENSE_ICON_BASE_URI}/by-4.0.png'},
 
     'http://creativecommons.org/licenses/by-sa/4.0/': {
         'order': 2,
-        'label': 'Creative Commons Attribution-ShareAlike license',
+        'label': 'CC BY-SA: Creative Commons Attribution-ShareAlike',
+        'note': 'This license allows reusers to distribute, remix, adapt, and '
+                'build upon the material in any medium or format, so long as '
+                'attribution is given to the creator. The license allows for '
+                'commercial use. If you remix, adapt, or build upon the '
+                'material, you must license the modified material under '
+                'identical terms.',
         'is_current': True,
         'icon_uri': f'{LICENSE_ICON_BASE_URI}/by-sa-4.0.png'},
 
     'http://creativecommons.org/licenses/by-nc-sa/4.0/': {
         'order': 3,
-        'label': 'Creative Commons Attribution-Noncommercial-ShareAlike '
-                 'license',
+        'label': 'CC BY-NC-SA: Creative Commons '
+                 'Attribution-Noncommercial-ShareAlike ',
+        'note': 'This license allows reusers to distribute, remix, adapt, and '
+                'build upon the material in any medium or format for '
+                'noncommercial purposes only, and only so long as attribution '
+                'is given to the creator. If you remix, adapt, or build upon '
+                'the material, you must license the modified material under '
+                'identical terms.',
         'is_current': True,
         'icon_uri': f'{LICENSE_ICON_BASE_URI}/by-nc-sa-4.0.png'},
 
     'http://creativecommons.org/licenses/by-nc-nd/4.0/': {
         'order': 4,
-        'label': 'Creative Commons Attribution-Noncommercial-NoDerivatives '
-                 'license',
+        'label': 'CC BY-NC-ND: Creative Commons '
+                 'Attribution-Noncommercial-NoDerivatives',
+        'note': 'This license allows reusers to copy and distribute the '
+                'material in any medium or format in unadapted form only, for '
+                'noncommercial purposes only, and only so long as attribution '
+                'is given to the creator.',
         'is_current': True,
         'icon_uri': f'{LICENSE_ICON_BASE_URI}/by-nc-nd-4.0.png'},
 
     'http://arxiv.org/licenses/nonexclusive-distrib/1.0/': {
         'label': 'arXiv.org perpetual, non-exclusive license to '
                  'distribute this article',
-        'note': '(Minimal rights required by arXiv.org. Select this '
-                'unless you understand the implications of other '
-                'licenses)',
+        'note': 'This license gives limited rights to arXiv to distribute the '
+                'article, and limits re-use of any type from other entities '
+                'or individuals.',
         'order': 5,
         'is_current': True,
     },
 
     'http://creativecommons.org/publicdomain/zero/1.0/': {
-        'label': 'Creative Commons Public Domain Declaration',
+        'label': 'CC Zero: No Rights Reserved',
+        'note': 'CC Zero is a public dedication tool, which allows creators '
+                'to give up their copyright and put their works into the '
+                'worldwide public domain. CC0 allows reusers to distribute, '
+                'remix, adapt, and build upon the material in any medium or '
+                'format, with no conditions.',
         'order': 6,
         'is_current': True,
         'icon_uri': f'{LICENSE_ICON_BASE_URI}/zero-1.0.png'
