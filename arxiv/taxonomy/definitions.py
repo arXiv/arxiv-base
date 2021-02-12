8f6e5cbdcbb1ef5cabee8b72e04d85cfa5de5b3e
Jake Weiskoff <jsw73@cornell.edu>
2021-02-12 13:54:03 -0600

    added hep-ph

--- arxiv/taxonomy/definitions.py
+++ arxiv/taxonomy/definitions.py
@@ -1179,6 +1179,11 @@ CATEGORIES = {
     'hep-ph': {
         'name': 'High Energy Physics - Phenomenology',
         'in_archive': 'hep-ph',
+        'description': 'Theoretical particle physics and its interrelation '
+                       'with experiment. Prediction of particle physics '
+                       'observables: models, effective field theories, '
+                       'calculation techniques. Particle physics: analysis of '
+                       'theory through experimental results.',
         'is_active': True,
         'is_general': False
     },
