4c69e0f4bc2260fd6153c74f1fa0b2d5a419919f
kyokukou <erin@arxiv.org>
2024-03-15 12:47:24 -0700

    address circular imports incorporate get cannonical display

--- arxiv/taxonomy/category.py
+++ arxiv/taxonomy/category.py
@@ -3,23 +3,12 @@ from typing import Optional, List
 from datetime import date
 from pydantic import BaseModel
 
-from .definitions import CATEGORIES, ARCHIVES_SUBSUMED, CATEGORY_ALIASES, \
-    ARCHIVES, GROUPS
-
 class BaseTaxonomy(BaseModel):
     id: str
     full_name: str
     is_active: bool
-    alt_name: Optional[str] #if part of an alias or subsumed archive pair
+    alt_name: Optional[str] #if part of an alias or subsumed archive pair displays the other name the category may be known as
     _alt_canonical: Optional[str] #private, use canonical method
-
-    @property
-    def display(self) -> str:
-        """
-        Output the display string for a category.
-        Example: Earth and Planetary Astrophysics (astro-ph.EP)
-        """
-        return f'{self.full_name} ({self.id})'
     
     @property
     def canonical(self) -> str:
@@ -29,6 +18,29 @@ class BaseTaxonomy(BaseModel):
         In the case of alias pairs returns the canonical category.
         """
         return self._alt_canonical if self._alt_canonical else self.id
+    
+    def display(self, canonical: bool = True) -> str:
+        """
+        Output the display string for a category.
+        If canonical, always display the canonical name
+        Example: Earth and Planetary Astrophysics (astro-ph.EP)
+        """
+        from .definitions import CATEGORIES,ARCHIVES, GROUPS
+        if not canonical:
+            return f'{self.full_name} ({self.id})'
+        else:
+            name=self.canonical 
+            if name==self.id:
+                return f'{self.full_name} ({self.id})'
+            elif name in CATEGORIES.keys():
+                item=CATEGORIES[name]
+                return f'{item.full_name} ({item.id})'
+            elif name in ARCHIVES.keys():#will never happen with taxonomy as of 2024
+                item=ARCHIVES[name]
+                return f'{item.full_name} ({item.id})'
+            elif name in GROUPS.keys(): #will never happen with taxonomy as of 2024
+                item=GROUPS[name]
+                return f'{item.full_name} ({item.id})'
 
 class Group(BaseTaxonomy):
     """Represents an arXiv group--the highest (most general) taxonomy level."""
@@ -38,8 +50,8 @@ class Group(BaseTaxonomy):
 
     def get_archives(self) -> List['Archive'] :
         """creates a list of all archives withing the group"""
+        from .definitions import ARCHIVES
         return [archive for archive in ARCHIVES.values() if archive.in_group == self.id]
-
 
 class Archive(BaseTaxonomy):
     """Represents an arXiv archive--the middle level of the taxonomy."""
@@ -50,10 +62,12 @@ class Archive(BaseTaxonomy):
 
     def get_group(self) -> Group:
         """Returns parent archive."""
+        from .definitions import GROUPS
         return GROUPS[self.in_group]
     
     def get_categories(self) -> List['Category'] :
         """creates a list of all categories withing the group"""
+        from .definitions import CATEGORIES,ARCHIVES, GROUPS
         return [category for category in CATEGORIES.values() if category.in_archive == self.id]
 
 class Category(BaseTaxonomy):
@@ -65,4 +79,5 @@ class Category(BaseTaxonomy):
 
     def get_archive(self) -> Archive:
         """Returns parent archive."""
+        from .definitions import ARCHIVES
         return ARCHIVES[self.in_archive]
