2635561e6b00895f2f54afe8ec363198a071cd19
Brian D. Caruso <bdc34@cornell.edu>
2024-03-19 15:12:51 -0400

    Puts AbsException as super of other abs exceptions

--- arxiv/document/exceptions.py
+++ arxiv/document/exceptions.py
@@ -2,17 +2,17 @@ class AbsException(Exception):
     """Error class for general arXiv .abs exceptions."""
 
 
-class AbsNotFoundException(FileNotFoundError):
+class AbsNotFoundException(FileNotFoundError, AbsException):
     """Error class for arXiv .abs file not found exceptions."""
 
 
-class AbsVersionNotFoundException(FileNotFoundError):
+class AbsVersionNotFoundException(FileNotFoundError, AbsException):
     """Error class for arXiv .abs file version not found exceptions."""
 
 
-class AbsParsingException(OSError):
+class AbsParsingException(OSError, AbsException):
     """Error class for arXiv .abs file parsing exceptions."""
 
 
-class AbsDeletedException(Exception):
+class AbsDeletedException(AbsException):
     """Error class for arXiv papers that have been deleted."""
