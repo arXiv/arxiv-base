3d4a69e9956da5cb9cb200b9267097ef4fb21187
Brian D. Caruso <bdc34@cornell.edu>
2025-05-05 14:14:09 -0400

    WIP Adds a funciton needed in the prevoius commit

--- arxiv/__init__.py
+++ arxiv/__init__.py
@@ -1,1 +1,11 @@
 """Arxiv pacakges from arxiv-base."""
+
+import contextvars
+
+
+def is_in_flask_context():
+    """Checks if currently in a Flask context without importing flask."""
+    return any(
+        var.name in {'app', 'request'}
+        for var in contextvars.copy_context()
+    )
