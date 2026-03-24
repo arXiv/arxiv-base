b6d0a5e146cac482cc30f0e111cbf4741ccf7ac3
Jonathan Young <jonathanhyoung@users.noreply.github.com>
2026-03-24 09:20:44 -0400

    QA-100 improve unit tests.

--- arxiv/metadata/tests/test_checksum.py
+++ arxiv/metadata/tests/test_checksum.py
@@ -13,4 +13,18 @@ def test_checksum_metadata():
                 self.msc_class="bogus value"
                 self.acm_class="bogus value"
 
-    assert checksum_metadata(Md1())
+    class Md2:
+        def __init__(self):
+                self.title="another value"
+                self.authors="another value"
+                self.abstract="another value"
+                self.comments="another value"
+                self.report_num="another value"
+                self.journal_ref="another value"
+                self.doi="another value"
+                self.msc_class="another value"
+                self.acm_class="another value"
+                
+    assert checksum_metadata(Md1()) == checksum_metadata(Md1())
+    assert checksum_metadata(Md2()) == checksum_metadata(Md2())
+    assert checksum_metadata(Md1()) != checksum_metadata(Md2())
