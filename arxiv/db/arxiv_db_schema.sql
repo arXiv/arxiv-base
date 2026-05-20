772089077c295d6cfc0a6dcc4c32bc34ba9354d6
Norbert Preining <norbert@arxiv.org>
2026-05-20 23:29:04 +0200

    Switch from latin1 to utf8mb4

--- arxiv/db/arxiv_db_schema.sql
+++ arxiv/db/arxiv_db_schema.sql
@@ -1083,7 +1083,7 @@ CREATE TABLE `arXiv_holidays` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`freeze_skip_date`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `arXiv_publish_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
