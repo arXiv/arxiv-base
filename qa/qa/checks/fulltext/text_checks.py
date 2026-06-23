af6c9bd92f52d1630d488f4e21d00d5ad5d8e926
Jonathan Young <jonathanhyoung@users.noreply.github.com>
2026-06-23 12:26:25 -0400

    QA-148 Full text checks

---
+++ qa/qa/checks/fulltext/text_checks.py
@@ -0,0 +1,52 @@
+
+from qa.checks.base import BaseCheck
+
+from qa.checks.models import OnFailurePolicy, QaDataRegistry, Metadata, Result
+
+
+class MissingTextCheck(BaseCheck):
+    """Check for missing text (probably PDF invalid or password protected)."""
+
+    name = "missing_text"
+    display_name = "Missing Text"
+    id = 14
+    version = "1.0.0"
+    description = "Failed to extract text."
+    on_failure_policy = OnFailurePolicy.REJECT
+    failure_message = "Missing text (check PDF?)"
+
+    required_inputs = {"fulltext"}
+
+    def _run(self, data_registry: QaDataRegistry) -> Result:
+        fulltext = data_registry.fulltext
+        if fulltext is not None and fulltext != "":
+            passed = True
+            return self._result(passed)
+        else:
+            passed = False
+            return self._result(passed, message = self.failure_message)
+        
+
+class VeryShortTextCheck(BaseCheck):
+    """Check for very short text."""
+
+    name = "short_text"
+    display_name = "Short Text"
+    id = 15
+    version = "1.0.0"
+    description = "The full text extracted is too short."
+    on_failure_policy = OnFailurePolicy.REJECT
+    failure_message = "Very short text (< 1400 words). Check full text."
+    required_inputs = {"fulltext"}
+
+    def _run(self, data_registry: QaDataRegistry) -> Result:
+        fulltext = data_registry.fulltext
+        if len(fulltext) < 10000 and len(fulltext.split()) < 1400 :
+            passed = False
+            return self._result(passed, message = self.failure_message)
+        else:
+            passed = True
+            return self._result(passed)
+
+
+        
---
+++ qa/tests/checks/fulltext/test_text.py
@@ -0,0 +1,40 @@
+"""Tests for AbstractIsValid."""
+
+import pytest
+
+from qa.checks.base import MissingDataError
+from qa.checks.models import OnFailurePolicy, QaDataRegistry, Metadata, Result
+from qa.checks.fulltext.text_checks import (
+    MissingTextCheck,
+    VeryShortTextCheck,
+)
+
+class TestMissingText:
+    def test_pass_normal(self):
+        text = "In this work, we study aaa, bbb, and ccc and conclude ddd. "
+        result = MissingTextCheck().run(QaDataRegistry(fulltext=text))
+        assert result.passed
+        
+    def test_fail_on_none(self):
+        text = None
+        result = MissingTextCheck().run(QaDataRegistry(fulltext=text))
+        assert not result.passed
+
+    def test_fail_on_empty(self):
+        text = ""
+        result = MissingTextCheck().run(QaDataRegistry(fulltext=text))
+        assert not result.passed
+
+class TestVeryShortText:
+    def test_pass_normal(self):
+        text = "In this work, we study aaa, bbb, and ccc and conclude ddd. " * 140
+
+        result = VeryShortTextCheck().run(QaDataRegistry(fulltext=text))
+        assert result.passed
+        
+    def test_fail_nospaces(self):
+        text = "Inthiswork, westudyaaa, bbb, andcccandconcludeddd. " * 100
+        
+        result = VeryShortTextCheck().run(QaDataRegistry(fulltext=text))
+        assert not result.passed
+
