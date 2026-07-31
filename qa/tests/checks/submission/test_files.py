140c8f430e1da8b6b8f2540d75971b15374d4b27
Jonathan Young <jonathanhyoung@users.noreply.github.com>
2026-07-31 15:00:30 -0400

    Fix one file.

--- qa/tests/checks/submission/test_files.py
+++ qa/tests/checks/submission/test_files.py
@@ -3,7 +3,7 @@
 import pytest
 from qa.checks.base import MissingDataError
 from qa.checks.models import QaDataRegistry
-from qa.checks.submission.files import DoesNotExceedTheFileSizeLimit, AcceptableFileType
+from qa.checks.submission.files import DoesNotExceedTheFileSizeLimit
 from tests.utils import make_test_submit_event_info
 
 
@@ -23,21 +23,3 @@ class TestOversizeCheck:
     def test_missing_submit_event_info_raises(self):
         with pytest.raises(MissingDataError):
             DoesNotExceedTheFileSizeLimit().run(QaDataRegistry())
-
-
-class TestAcceptableFileType:
-    def test_filetype_pass(self):
-        sub_metadata = make_test_submit_event_info()
-        result = AcceptableFileType().run(QaDataRegistry(submit_event_info=sub_metadata))
-
-        assert result.passed
-
-    def test_filetype_fail(self):
-        sub_metadata = make_test_submit_event_info(source_format="html")
-        result = AcceptableFileType().run(QaDataRegistry(submit_event_info=sub_metadata))
-
-        assert not result.passed
-
-    def test_missing_submit_event_info_raises(self):
-        with pytest.raises(MissingDataError):
-            AcceptableFileType().run(QaDataRegistry())
