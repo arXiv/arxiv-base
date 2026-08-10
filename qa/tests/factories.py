f3804d190d7f052624f8bfc555c8c3deea9ce640
carly <carly@arxiv.org>
2026-08-10 14:35:34 -0400

    remove metadata_version from submit event info

--- qa/tests/factories.py
+++ qa/tests/factories.py
@@ -4,14 +4,12 @@ from qa.checks.models import SubmitEventInfo
 def make_test_submit_event_info(
     type="new",
     is_oversize=False,
-    metadata_version=23456,
     submitter_name="Donald Duck",
     source_format="tex",
 ):
     return SubmitEventInfo(
         type=type,
         is_oversize=is_oversize,
-        metadata_version=metadata_version,
         submitter_name=submitter_name,
         source_format=source_format,
     )
